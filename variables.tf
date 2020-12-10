ea178bc34cfa744ea2bc905f828f6d653d519de0
npc-allanb <allanb@newspaperclub.com>
2020-12-10 20:13:45 +0000

    update to put defaults in for variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "region" {
   description = "This is the cloud hosting region where your webapp will be deployed."
+  default = "eu-west-1"
 }
 
 variable "prefix" {
   description = "This is the environment your webapp will be prefixed with. dev, qa, or prod"
+  default = "dev"
 }
 
 variable "name" {
   description = "Your name to attach to the webapp address"
+  default = "allan"
 }
